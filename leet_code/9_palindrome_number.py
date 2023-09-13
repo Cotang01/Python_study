c1edf40ef7e7173a50f700396fdbc1fc7d5ddcbf
Cotang01 <127985804+Cotang01@users.noreply.github.com>
2023-09-13 23:02:37 +0500

    add 2nd solution to 9 leet_code task

--- leet_code/9_palindrome_number.py
+++ leet_code/9_palindrome_number.py
@@ -4,11 +4,11 @@ palindrome, and false otherwise.
 """
 
 
-def is_palindrome(n: int) -> bool:
-    if n < 0:
+def is_palindrome_str(x: int) -> bool:
+    if x < 0:
         return False
     else:
-        n = str(n)
+        n = str(x)
         left = 0
         right = len(n) - 1
         while left < right and left != right:
@@ -18,3 +18,15 @@ def is_palindrome(n: int) -> bool:
                 left += 1
                 right -= 1
         return True
+
+
+def is_palindrome_int(x: int) -> bool:
+    if x < 0:
+        return False
+    else:
+        x_buffer = x
+        comparable = 0
+        while x > 0:
+            comparable = comparable * 10 + x % 10
+            x //= 10
+        return x_buffer == comparable
