1d424e3d621772db508dfd9f2edddb4d2ca4f8fc
Cotang01 <127985804+Cotang01@users.noreply.github.com>
2024-02-18 08:14:03 +0500

    rework of __add__ and __sub__ Rectangle (its bad)

--- immersion_in_python/sem11/task5.py
+++ immersion_in_python/sem11/task5.py
@@ -32,32 +32,46 @@ class Rectangle:
             return self.side ** 2
         return self.width * self.length
 
-    def increase(self, *sides):
-        n = len(sides)
-        if n == 1:
-            new_side = self.side + sides[0]
-            return Rectangle(new_side)
-        elif n == 2:
-            new_width = self.width + sides[0]
-            new_length = self.length + sides[1]
-            return Rectangle(new_width, new_length)
-        else:
-            raise ValueError(
-                f"{self.__class__} cannot have {n} sides")
+    def __add__(self, other):
+        if hasattr(self, 'side') and hasattr(other, 'side'):
+            return Rectangle(self.side + other.side)
+        if not hasattr(self, 'side') and hasattr(other, 'side'):
+            return Rectangle(self.width + other.side,
+                             self.length + other.side)
+        if hasattr(self, 'side') and not hasattr(other, 'side'):
+            return Rectangle(self.side + other.width,
+                             self.side + other.length)
+        if not hasattr(self, 'side') and not hasattr(other, 'side'):
+            return Rectangle(self.width + other.width,
+                             self.length + other.length)
 
-    def decrease(self, *sides):
-        n = len(sides)
-        if n == 1:
-            new_side = self.side - sides[0]
+    def __sub__(self, other):
+        if hasattr(self, 'side') and hasattr(other, 'side'):
+            new_side = self.side - other.side
             if new_side < 0:
-                raise ValueError('Sides cannot have negative len')
-            return Rectangle(new_side)
-        elif n == 2:
-            new_width = self.width - sides[0]
-            new_length = self.length - sides[1]
-            if new_width < 0 or new_length < 0:
-                raise ValueError('Sides cannot have negative len')
+                raise ValueError(f'New side {new_side} should be positive')
+            return Rectangle(self.side - other.side)
+        if not hasattr(self, 'side') and hasattr(other, 'side'):
+            new_width = self.width - other.side
+            if new_width < 0:
+                raise ValueError(f'New width {new_width} should be positive')
+            new_length = self.length - other.side
+            if new_length < 0:
+                raise ValueError(f'New length {new_length} should be positive')
             return Rectangle(new_width, new_length)
-        else:
-            raise ValueError(
-                f"{self.__class__} cannot have {n} sides")
+        if hasattr(self, 'side') and not hasattr(other, 'side'):
+            new_width = self.side - other.width
+            if new_width < 0:
+                raise ValueError(f'New width {new_width} should be positive')
+            new_length = self.side - other.length
+            if new_length < 0:
+                raise ValueError(f'New length {new_length} should be positive')
+            return Rectangle(new_width, new_length)
+        if not hasattr(self, 'side') and not hasattr(other, 'side'):
+            new_width = self.width - other.width
+            if new_width < 0:
+                raise ValueError(f'New width {new_width} should be positive')
+            new_length = self.length - other.length
+            if new_length < 0:
+                raise ValueError(f'New length {new_length} should be positive')
+            return Rectangle(new_width, new_length)
