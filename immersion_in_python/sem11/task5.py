09d0b055d3496ede8de299350cd15ee3907862ed
Cotang01 <127985804+Cotang01@users.noreply.github.com>
2024-02-23 17:40:33 +0500

    class optimizations for sem12/task4

--- immersion_in_python/sem11/task5.py
+++ immersion_in_python/sem11/task5.py
@@ -12,66 +12,28 @@ class Rectangle:
     """Class for creating Rectangle objects"""
 
     def __init__(self, *sides):
-        n = len(sides)
-        if n == 1:
-            self.side = sides[0]
-        elif n == 2:
-            self.width = sides[0]
-            self.length = sides[1]
-        else:
-            raise ValueError(
-                f"{self.__class__} cannot have {n} sides")
+        match len(sides):
+            case 1 | 2:
+                self.width = sides[0]
+                self.length = sides[-1]
+                self.perimeter = (self.width + self.length) * 2
+                self.area = self.width * self.length
+            case _:
+                raise ValueError(
+                    f'{self.__class__.__name__} '
+                    f'cannot have that amount of sides'
+                )
 
-    def get_per(self):
-        if hasattr(self, 'side'):
-            return self.side * 4
-        return (self.width + self.length) * 2
-
-    def get_area(self):
-        if hasattr(self, 'side'):
-            return self.side ** 2
-        return self.width * self.length
+    def _is_valid_size(self, value):
+        if value < 0:
+            raise ValueError('Side cannot have negative length')
+        return True
 
     def __add__(self, other):
-        if hasattr(self, 'side') and hasattr(other, 'side'):
-            return Rectangle(self.side + other.side)
-        if not hasattr(self, 'side') and hasattr(other, 'side'):
-            return Rectangle(self.width + other.side,
-                             self.length + other.side)
-        if hasattr(self, 'side') and not hasattr(other, 'side'):
-            return Rectangle(self.side + other.width,
-                             self.side + other.length)
-        if not hasattr(self, 'side') and not hasattr(other, 'side'):
-            return Rectangle(self.width + other.width,
-                             self.length + other.length)
+        return Rectangle(self.width + other.width, self.length + other.length)
 
     def __sub__(self, other):
-        if hasattr(self, 'side') and hasattr(other, 'side'):
-            new_side = self.side - other.side
-            if new_side < 0:
-                raise ValueError(f'New side {new_side} should be positive')
-            return Rectangle(self.side - other.side)
-        if not hasattr(self, 'side') and hasattr(other, 'side'):
-            new_width = self.width - other.side
-            if new_width < 0:
-                raise ValueError(f'New width {new_width} should be positive')
-            new_length = self.length - other.side
-            if new_length < 0:
-                raise ValueError(f'New length {new_length} should be positive')
-            return Rectangle(new_width, new_length)
-        if hasattr(self, 'side') and not hasattr(other, 'side'):
-            new_width = self.side - other.width
-            if new_width < 0:
-                raise ValueError(f'New width {new_width} should be positive')
-            new_length = self.side - other.length
-            if new_length < 0:
-                raise ValueError(f'New length {new_length} should be positive')
-            return Rectangle(new_width, new_length)
-        if not hasattr(self, 'side') and not hasattr(other, 'side'):
-            new_width = self.width - other.width
-            if new_width < 0:
-                raise ValueError(f'New width {new_width} should be positive')
-            new_length = self.length - other.length
-            if new_length < 0:
-                raise ValueError(f'New length {new_length} should be positive')
-            return Rectangle(new_width, new_length)
+        if not self._is_valid_size(new_w := self.width - other.width) \
+                or not self._is_valid_size(new_l := self.length - other.length):
+            raise ValueError(f'New {self.__class__} cannot have negative size')
+        return Rectangle(new_w, new_l)
